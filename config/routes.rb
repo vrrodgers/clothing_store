7cf97c54c6627bb16f4bf9a98965f38ce65ba683
vrrodgers <trulymissvicky1982@gmail.com>
2019-06-17 13:24:33 -0500

    added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'products#index'
   resources :products
-
+  
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'cart', to: 'pages#cart'
 end

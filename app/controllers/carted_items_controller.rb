9cc735c46f624be4ff6da7957b6efba58da4a81e
vrrodgers <trulymissvicky1982@gmail.com>
2019-08-02 14:38:07 -0500

    made changes

--- app/controllers/carted_items_controller.rb
+++ app/controllers/carted_items_controller.rb
@@ -26,4 +26,19 @@ class CartedItemsController < ApplicationController
        
     end
 
+    def add_item(product_id)
+      item = items.where('product_id = ?', product_id).first
+      if item
+        # increase the quantity of product in cart
+        item.quantity + 1
+        save
+      else
+        # product does not exist in cart
+        cart.items << Item.new(product_id: product_id, quantity: 1)
+       end
+       save
+    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'products#index'
   resources :products
+  post '/add_to_cart/:product_id' => 'cart#add_to_cart', :as => 'add_to_cart'
+
   
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
